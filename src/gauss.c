eddefb468deca87c310ee63afe73fb7193547b98
Lukasz <lukis30888@gmail.com>
2021-01-02 17:25:57 +0100

    Wyliczanie wartosci skalaru

--- src/gauss.c
+++ src/gauss.c
@@ -1,13 +1,21 @@
 #include "gauss.h"
+#include <stdio.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+	int w,k;
+	for (k = 0;k < mat->c;k++)
+	{
+		for (int w = k + 1;w < mat->r;w++)
+		{
+			double skalar = mat->data[k][k]/mat->data[w][k];
+				printf("wsp:%f, ", skalar);
+		}
+		printf("\n");
+	}
 
 		return 0;
 }
