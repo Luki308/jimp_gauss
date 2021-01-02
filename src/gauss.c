d1b4608ea91e82e2433729ff1310691652b8a955
Lukasz <lukis30888@gmail.com>
2021-01-02 19:24:32 +0100

    Dodanie warunku, gdy macierz jest osobliwa

--- src/gauss.c
+++ src/gauss.c
@@ -10,12 +10,18 @@ int eliminate(Matrix *mat, Matrix *b){
 	int w,k;
 	for (k = 0;k < mat->c;k++)
 	{
-		for (int w = k + 1;w < mat->r;w++)
+		if (mat->data[k][k] == 0)
 		{
-			double skalar = mat->data[w][k]/mat->data[k][k];
-			for (int i = k;i < mat->c;i++)
+			return 1;
+		}
+		else {
+			for (int w = k + 1;w < mat->r;w++)
 			{
-				mat->data[w][i]=mat->data[w][i] - skalar * mat->data[k][i];
+				double skalar = mat->data[w][k] / mat->data[k][k];
+				for (int i = k;i < mat->c;i++)
+				{
+					mat->data[w][i] = mat->data[w][i] - skalar * mat->data[k][i];
+				}
 			}
 		}
 	}
