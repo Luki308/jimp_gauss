e3f5dc1621fa3ba67d926513bbe1dd78574c9f62
Lukasz <lukis30888@gmail.com>
2021-01-03 15:47:45 +0100

    Implementacja wyboru największego współczynnika i zmiana Makefile

--- src/gauss.c
+++ src/gauss.c
@@ -7,24 +7,39 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
+	int k, w;
 	for (k = 0;k < mat->c;k++)
 	{
-		int max,wmax;
+		double max;
+		int wmax;
 		wmax = k;
-		max = mat->data[k][k];
+		max = fabs(mat->data[k][k]);
+
+		for (w = k + 1;w < mat->r;w++)
+		{
+			if (max < fabs(mat->data[w][k]))
+			{
+				max = fabs(mat->data[w][k]);
+				wmax = w;
+			}
+			printf("max: %f", max);
+		}
+
 		if (mat->data[k][k] == 0)
 		{
 			return 1;
 		}
 		else {
-			for (int w = k + 1;w < mat->r;w++)
+			for (w = k + 1;w < mat->r;w++)
 			{
 				double skalar = mat->data[w][k] / mat->data[k][k];
-				for (int i = k;i < mat->c;i++)
+				int i;
+				for (i = k;i < mat->c;i++)
 				{
 					mat->data[w][i] = mat->data[w][i] - skalar * mat->data[k][i];
 				}
 				b->data[w][0] = b->data[w][0] - skalar * b->data[k][0];
+				printToScreen(mat);
 			}
 		}
 	}
